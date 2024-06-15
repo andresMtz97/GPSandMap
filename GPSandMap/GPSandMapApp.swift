026cbcdae63562e055233468fa06a4cbd2171188
Andrés Martínez <andres.martinez@proveeduria.unam.mx>
2024-06-15 09:43:46 -0600

    Initial Commit

---
+++ GPSandMap/GPSandMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSandMapApp.swift
+//  GPSandMap
+//
+//  Created by DISMOV on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GPSandMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
